224fcef0cf52a73838c2375f720d9952b1191f71
edlobox <loboxed@gmail.com>
2017-06-09 11:50:45 -0500

    adding update and destroy for Todo

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const todosController = require('../controllers').todos;
-const todoItemsController = require('../controllers').todoitems;
+const todoItemsController = require('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -9,4 +9,8 @@ module.exports = (app) => {
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
   app.post('/api/todos/:todoId/items', todoItemsController.create);
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
+
 };
